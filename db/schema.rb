a1a7c15e398ee77efae5a0ecb9028faf1c64ed89
johnlangs <me@johnlangs.dev>
2024-09-16 00:33:28 +0000

    added code to subject model (ie "CSCE")

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_16_001735) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_16_003038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_16_001735) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "code"
   end
 
   add_foreign_key "course_prerequisites", "courses"
