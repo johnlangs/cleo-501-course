6306d0e9d9e068425088c0744a8cccca67849446
root <root@localhost>
2024-09-20 00:32:28 +0000

    Updated courses index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,7 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
-    @courses = Course.all
+    @courses = Course.includes(:subject)
   end
 
   # GET /courses/1 or /courses/1.json
