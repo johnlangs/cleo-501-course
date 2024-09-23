a4170d7e20bb1addd4954ea4709c9e92210d8f61
Andreas <aleefsen@gmail.com>
2024-09-23 04:10:13 +0000

    Shows oath uid now

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,9 @@
 class DashboardsController < ApplicationController
+    before_action :authenticate_admin!
+  
     def show
+      @oauth_uid = session[:oauth_uid]  # Get the OAuth UID from the session
+      @admin = current_admin
     end
-end
+  end
+  
