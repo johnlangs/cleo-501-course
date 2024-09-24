873a6089dd32d2b3ac735d98c1306f4b06d5f02a
root <Matthew.gorena@gmail.com>
2024-09-24 20:34:30 +0000

    integration test for google oauth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+
+require 'omniauth'
+OmniAuth.config.test_mode = true
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
